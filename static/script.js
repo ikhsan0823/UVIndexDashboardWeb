a902c89b80b416c23cd8b392d329575f242f5366
Ikhsan <ikhsanhumaerah04@gmail.com>
2023-12-10 00:43:19 +0800

    Initial Update

--- static/script.js
+++ static/script.js
@@ -7,3 +7,47 @@ document.addEventListener("DOMContentLoaded", function () {
         container.style.display = (container.style.display === "none" || container.style.display === "") ? "block" : "none";
     });
 });
+
+const x = document.getElementById("demo");
+const apiKey = 'b4a44113af2c49eba6294c002abd4997';  // Replace with your Geoapify API key
+const latitudeInput = document.getElementById("latitude");
+const longitudeInput = document.getElementById("longitude");
+function getUVIndexWithCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    latitudeInput.value = position.coords.latitude;
+    longitudeInput.value = position.coords.longitude;
+
+    const apiUrl = `https://api.geoapify.com/v1/geocode/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&type=city&lang=en&limit=1&format=json&apiKey=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(result => {
+            const formattedAddress = result.results[0].formatted;
+            document.getElementById('address').value = formattedAddress;
+        })
+        .catch(error => console.log('error', error));
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred.";
+            break;
+    }
+}
